86e42af7aadd91cba952fe1a30ce565fb32698c6
Troy David Cook <www.troydavidcook@gmail.com>
2018-01-24 14:33:28 -0700

    create and implement comment routes for BTS images.

---
+++ routes/bts_comments.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const middleware = require('../middleware');
+const Comment = require('../models/comments');
+const BtsImage = require('../models/bts_images');
+
+const router = express.Router({ mergeParams: true });
+
+// New comment route
+// router.get('/new', middleware.isLoggedIn, (req, res) => {
+//   const imageId = req.params.id;
+//   Image.findById(imageId, (err, image) => {
+//     if (err) {
+//       console.log('Error: ', err);
+//     } else {
+//       res.render('comments/new', { image });
+//     }
+//   });
+// });
+
+// Important association/ Create new comment route
+
+router.post('/', middleware.isLoggedIn, (req, res) => {
+  const imageId = req.params.id;
+  BtsImage.findById(imageId, (err, image) => {
+    if (err) {
+      console.log('Error: ', err);
+      res.redirect('/bts_images');
+    } else {
+      // Posting and assocciation.
+      Comment.create(req.body.comment, (error, comment) => {
+        if (err) {
+          console.log('Error: ', error);
+        } else {
+          // add username and id to comment, then save.
+          comment.author.id = req.user._id;
+          comment.author.username = req.user.username;
+          // save comment
+          comment.save();
+          image.comments.push(comment);
+          image.save();
+          req.flash('success', 'Successfully created comment.');
+          res.redirect(`/bts_images/${imageId}`);
+        }
+      });
+    }
+  });
+});
+
+router.get('/:comment_id/edit', middleware.checkCommentOwnership, (req, res) => {
+  const commentId = req.params.comment_id;
+  Comment.findById(commentId, (err, fetchedComment) => {
+    if (err) {
+      console.log('Error: ', err);
+      res.redirect('back');
+    } else {
+      res.render('comments/bts_edit', { bts_image_id: req.params.id, comment: fetchedComment });
+      // Comment edit route
+    }
+  });
+});
+
+// Comment UPDATE route
+router.put('/:comment_id', middleware.checkCommentOwnership, (req, res) => {
+  const commentId = req.params.comment_id;
+  const btsImageId = req.params.id;
+  Comment.findByIdAndUpdate(commentId, req.body.comment, (err, updatedComment) => {
+    if (err) {
+      res.redirect('back');
+    } else {
+      res.redirect(`/bts_images/${btsImageId}`);
+    }
+  });
+});
+
+// Comment destroy
+router.delete('/:comment_id', middleware.checkCommentOwnership, (req, res) => {
+  const commentId = req.params.comment_id;
+  const btsImageId = req.params.id;
+  Comment.findByIdAndRemove(commentId, (err) => {
+    if (err) {
+      res.redirect('back');
+    } else {
+      req.flash('success', 'Comment Deleted.');
+      res.redirect(`/bts_images/${btsImageId}`);
+    }
+  });
+});
+
+module.exports = router;
