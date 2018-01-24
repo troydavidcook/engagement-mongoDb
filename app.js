32cde8e63f36f660f04a1aa980ac22fe4b2b2142
Troy David Cook <www.troydavidcook@gmail.com>
2018-01-24 15:22:47 -0700

    change env to herokudb

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ const commentRoutes = require('./routes/comments');
 const app = express();
 
 mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://troydavidcook:l0gical1@ds111598.mlab.com:11598/k-d-proposal', { useMongoClient: true });
-mongoose.connect('mongodb://localhost/engagementk&d', { useMongoClient: true });
+mongoose.connect('mongodb://troydavidcook:l0gical1@ds111598.mlab.com:11598/k-d-proposal', { useMongoClient: true });
+// mongoose.connect('mongodb://localhost/engagementk&d', { useMongoClient: true });
 
 // seedImages();
 // seedBtsImages();
